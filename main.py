4eb94ced5648ffba6485169766f0fef2ac8d6fbe
sgczeslaw <111303837+sgczeslaw@users.noreply.github.com>
2022-09-22 15:00:03 +0200

    Update main.py

--- main.py
+++ main.py
@@ -20,15 +20,11 @@ username_for = low_word
 password_for = low_word + upper_word + number + symbols
 long_password = 16
 long_username = 12
-with open("proxy.txt") as f:
-    lines = f.readlines()
-PROXY = "http://IH77e2FzmChhhqAy:0kDHgHxlF35miImDv@"+random.choice(lines)
 options.add_argument('--no-first-run --no-service-autorun --password-store=basic') #wlacz to jak juz nie bedzie dev test
 options.user_data_dir = "rawr"
 options.add_argument("--window-size=1920,1080")
 options.add_argument('--user-data-dir=rawr')
 options.add_argument("--remote-debugging-port=38223")
-options.add_argument('--proxy-server=%s' % PROXY)
 driver = uc.Chrome(options=options, version_main=105)  # version_main allows to specify your chrome version instead of following chrome global version
 driver.set_window_size(1920, 1080)
 print("rawr")
